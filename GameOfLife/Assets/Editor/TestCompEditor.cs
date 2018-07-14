b85ffff9c8a30415278aed4c6399beb33c54421a
jasonchan35 <jasonchan35@gmail.com>
2018-07-14 21:47:16 +0200

    GameOfLife

---
+++ GameOfLife/Assets/Editor/TestCompEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TestComp))]
+[CanEditMultipleObjects]
+public class TestCompEditor : Editor {
+    public void OnSceneGUI()
+    {
+        var t = target as TestComp;
+        EditorGUI.BeginChangeCheck();
+        Vector3 scale = Handles.ScaleHandle(t.transform.localScale, Vector3.zero, Quaternion.identity, 1);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Scaled ScaleAt Point");
+            t.transform.localScale = scale;
+        }
+    }
+}
---
+++ GameOfLife/Assets/TestComp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestComp : MonoBehaviour {
+
+	public int test;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
